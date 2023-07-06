493ac4307357e97fe8537d54accd5c90eb6b3140
petro-lesapiti <petrolesapiti@gmail.com>
2023-07-06 15:46:27 -0700

    restructure the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import searchImage from './search.svg';
+import MovieCard from './movieCard';
 
 function App() {
+  const apiKey = '2af14ba0';
+  const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}`;
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    if (searchTerm.trim() !== '') {
+      searchMovies(searchTerm);
+    } else {
+      searchMovies('');
+    }
+  }, [searchTerm]);
+  
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
+
+    if (data.Response === 'True') {
+      setMovies(data.Search);
+    } else {
+      setMovies([]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-16 flex justify-center items-center flex-col ">
+      <h1 className='text-3xl antialiased'>
+        MovieLand
+        </h1>
+      <div className="w-71 mx-auto my-8 flex items-center justify-center p-6 rounded-full bg-gray-900 shadow-lg">
+        <input className='flex-1 border-none text-lg font-raleway font-medium pr-4 outline-none text-gray-500 bg-gray-900" type="text'
+          placeholder="Search Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={searchImage}
+          alt="search image"
+          onClick={() => setSearchTerm(searchTerm)}
+        />
+      </div>
+      <div className="container">
+        {movies.length > 0 || searchTerm.trim() === '' ? (
+          movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))
+        ) : (
+          <div className="empty">No movies</div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/movieCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function MovieCard({movie}) {
+  return (
+    <div className='movie'>
+          <div>
+            <p>{movie.Year}</p>
+          </div>
+          <div>
+            <img src={movie.Poster} alt=''></img>
+          </div>
+          <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+          </div>
+        </div>
+  )
+}
+
+export default MovieCard
